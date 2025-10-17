db24de5c2bde64220262c580914b6535c1ca2fcb
EimyCaicedo03 <eimycaicedo23@gmail.com>
2025-10-17 17:21:38 -0500

    avance 2

---
+++ escenario interactivo/app.js
@@ -0,0 +1,11 @@
+let contador = 0;
+let contadorEl = document.getElementById("contador");
+let gasolinas = document.querySelectorAll(".gasolina");
+
+gasolinas.forEach(g => {
+  g.addEventListener("click", () => {
+    g.style.display = "none";
+    contador++;
+    contadorEl.textContent = contador;
+  });
+});
